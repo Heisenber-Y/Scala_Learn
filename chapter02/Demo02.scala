090a21cb6e0a61f319c94bfbe4df016157cecc11
menglongye <menglongye1@gmail.com>
2019-07-31 18:15:40 +0800

    第2次提交

--- chapter02/Demo02.scala
+++ chapter02/Demo02.scala
@@ -7,12 +7,17 @@ object Demo02 {
 //20190730 11 aa
 //强制类型转换
 
-    val num1:Int=10*3.5.toInt+6*1.5.toInt
+/*    val num1:Int=10*3.5.toInt+6*1.5.toInt
     var num2:Int= (10*3.5+6*1.5).toInt
     println("num1="+num1 +","+"num2="+num2)
     var char1:Char=1
-    var num3=1
+    var num3=1*/
     //var char2:Char=num3  b报错
+
+    var s4="12.5"
+    //println(s4.toInt) //error
+    println(s4.toDouble)
+
 
 //Char 类型可以报错Int类型的常量值，但不能保存Int的变量值，需要强转
 //Byte和Short 类型在运算时，当作Int类型处理
---
+++ chapter03/Demo1.scala
@@ -0,0 +1,35 @@
+/**
+  * Created by yml on 2019/7/31.
+  */
+object Demo1 {
+  def main(args: Array[String]) {
+
+//11
+
+//  ++ 和--
+//在scala中没有++ 和-- 只有+=1 和-=1
+    var num1=10
+    //num1++  错误
+/*    num1+=1
+    println(num1)
+num1-=1
+    println(num1)*/
+
+
+// %的使用
+/*  println(10%3)
+    println(-10%3)*/
+
+
+
+    //  /的使用
+/*var r1:Int=10/3   //3
+    println(r1)
+var r2:Double=10/3
+    println(r2)
+    var r3:Double=10.0/3
+    println(r3.formatted("%.2f"))*/
+
+
+  }
+}
