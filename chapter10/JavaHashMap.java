c838a6bdd321c5c9da2e95b475cce1b58dcdcbd3
menglongye <menglongye1@gmail.com>
2019-08-08 18:04:37 +0800

    函数和面向对象编程

---
+++ chapter10/JavaHashMap.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yml on 2019/8/8.
+ */
+public class JavaHashMap {
+    public static void main(String[] args) {
+
+    }
+}
--- chapter10/QueueDemo01.scala
+++ chapter10/QueueDemo01.scala
@@ -11,7 +11,28 @@ object QueueDemo01 {
 println("q1="+q1)
     q1++=List(4,5,7)
     println("q1="+q1)
-//dequeue  20190807
+    //Queue 删除和假如队列元素
+//dequeue  从队列的头部取出数据 q1 本身会变
+  val dequeue: Int = q1.dequeue()
+    println("dequeue="+dequeue+"q1="+q1)
+    //enqueue 入队列 ，默认是从队列的尾部加入 redis
+    val enqueue: Unit = q1.enqueue(100,10,100,888)
+    println("q1="+q1)
+
+   println("======= 返回队列的元素")
+   //获取队列的第一个元素   对q1 没有影响
+    println(q1.head)
+    //获取队列的最后一个元素   对q1没有影响
+    println(q1.last)
+
+    //取出队尾的数据，即返回除了第一个意外的剩余的元素
+    println(q1.tail)
+    println(q1.tail)
+
+
 
   }
+
+
+
 }
